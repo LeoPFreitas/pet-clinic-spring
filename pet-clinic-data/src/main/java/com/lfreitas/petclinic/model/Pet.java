b97f2b534c4e7a5595fbb65d4ffa7c5ee312cae2
leonardo-w10 <leonardo.cadu2@gmail.com>
2020-09-17 16:47:35 -0300

    Add project lombok. Closes #50

--- pet-clinic-data/src/main/java/com/lfreitas/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/lfreitas/petclinic/model/Pet.java
@@ -1,11 +1,18 @@
 package com.lfreitas.petclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Table(name = "pets")
 public class Pet extends BaseEntity{
 
@@ -25,44 +32,4 @@ public class Pet extends BaseEntity{
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
     private Set<Visit> vists = new HashSet<>();
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public PetType getPetType() {
-        return petType;
-    }
-
-    public void setPetType(PetType petType) {
-        this.petType = petType;
-    }
-
-    public Owner getOwner() {
-        return owner;
-    }
-
-    public void setOwner(Owner owner) {
-        this.owner = owner;
-    }
-
-    public LocalDate getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(LocalDate birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    public Set<Visit> getVists() {
-        return vists;
-    }
-
-    public void setVists(Set<Visit> vists) {
-        this.vists = vists;
-    }
 }
