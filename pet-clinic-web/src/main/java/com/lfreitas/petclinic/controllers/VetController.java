34d8f2f7ed265c02470a19cfd4c81a3bb689f9d4
leonardo-w10 <leonardo.cadu2@gmail.com>
2020-09-18 15:57:54 -0300

    Add JUnit 5

--- pet-clinic-web/src/main/java/com/lfreitas/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/lfreitas/petclinic/controllers/VetController.java
@@ -1,7 +1,6 @@
 package com.lfreitas.petclinic.controllers;
 
 import com.lfreitas.petclinic.services.VetService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
